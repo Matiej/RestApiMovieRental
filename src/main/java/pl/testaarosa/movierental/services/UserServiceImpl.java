96f90b28a9b8ea8c6ecc0ab9c0747929f2d0fc17
Maciej <maciek@testaarosa.pl>
2018-06-05 02:30:52 +0200

    emailsFormCorrections

--- src/main/java/pl/testaarosa/movierental/services/UserServiceImpl.java
+++ src/main/java/pl/testaarosa/movierental/services/UserServiceImpl.java
@@ -49,10 +49,10 @@ public class UserServiceImpl implements UserService {
     private void sendEmail(UserForm userForm) {
         String subject = "Welcome " + userForm.getName() + ", Testaarosa movierental app created Yours account";
         StringBuilder message = new StringBuilder("Yours account created as above: " + "\n"
-                + "Name " + userForm.getName() + ", surname: " + userForm.getSurname() + "\n"
-                + "City: " + userForm.getCity() + ", street: " + userForm.getStreet() + "\n"
+                + "Name: " + userForm.getName() + ",  surname: " + userForm.getSurname() + "\n"
+                + "City: " + userForm.getCity() + ",  street: " + userForm.getStreet() + "\n"
                 + " ** Birthday: " +userForm.getBrithday() + "\n"
-                + "User email: " + userForm.getEmail() + " Register date: " + userForm.getRegisterDate() + "\n\n"
+                + "User email: " + userForm.getEmail() + " \n Date of registration: " + userForm.getRegisterDate() + "\n\n"
                 + "############################################## \n" + "YOU CAN FIND ME HERE:  http://www.testaarosa.pl");
                 ofNullable(userForm).ifPresent(c-> emailService.send(new Mail(
                 c.getEmail(), subject, message.toString(), adminConfig.getAdminMail())));
