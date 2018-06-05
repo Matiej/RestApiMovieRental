23ca5dc9abd70b81867f7ea5ad681f1d5aabf03b
Maciej <maciek@testaarosa.pl>
2018-06-05 21:23:04 +0200

    herokuHelp

--- src/main/java/pl/testaarosa/movierental/domain/User.java
+++ src/main/java/pl/testaarosa/movierental/domain/User.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String surname;
